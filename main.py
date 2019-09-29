35d8334072244d6a565d8bdb07621c39eb3c9fe6
Kamil <kemelzx@gmail.com>
2019-09-30 00:57:03 +0200

    Add main file

---
+++ main.py
@@ -0,0 +1,59 @@
+'no imports!'
+'no list comprehensions!'
+'no lambdas!'
+
+
+def get_max_common_element(first_list, second_list):
+    """
+    Select common elements from both lists and return one with the max value.
+
+    :raises ValueError: if any of the input lists are empty.
+    Error message: 'Input lists cannot be empty'
+ 
+    :raises ValueError: if there are no common elemens.
+    Error message: 'There are no common elements'
+    """
+    pass
+
+
+def get_odd_elements(x, start):
+    """
+    Return a list containing first 'x' odd elements starting from 'start'
+    """
+    pass
+
+
+def get_even_numbers(x, stop, z):
+    """
+    Returns a list containing first 'x' even elements lower than 'stop'.
+    That elements must be divisible by 'z'. 
+    """
+    pass
+
+
+def get_sum_of_greatest_elements(my_list, x):
+    """
+    Returns a single integer, which is a sum of 'x' biggest elements from 'my_list'
+    i.e. Returns a sum of 3 biggest elements from [2, 18, 5, -11, 7, 6, 9]
+    """
+    pass
+
+
+def get_average_of_elements(first_list, second_list):
+    """
+    Returns a single integer, which is an average from elements that are on 
+    'first_list' but not on 'second_list'
+    """
+    pass
+
+
+'BONUS'
+def return_prime_numbers_less_tahn_100():
+    """
+    Returns a list containing prime numbers that are less than 100
+    """
+    pass
+
+
+def main():
+    pass
